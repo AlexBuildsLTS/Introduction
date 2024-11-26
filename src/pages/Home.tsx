e6d90c1c73b0b4d00288f55208684d86e8c31986
Alex Youssef <84400592+AlexBuildsLTS@users.noreply.github.com>
2024-11-26 21:16:43 +0200

    Create Home page without MeetingCalendar component

---
+++ src/pages/Home.tsx
@@ -0,0 +1,22 @@
+// src/pages/Home.tsx
+
+import React from "react";
+import Hero from "../components/Hero";
+import About from "../components/About";
+import Skills from "../components/Skills";
+import Projects from "../components/Projects";
+import Contact from "../components/Contact";
+
+const Home: React.FC = () => {
+  return (
+    <>
+      <Hero />
+      <About />
+      <Skills />
+      <Projects />
+      <Contact />
+    </>
+  );
+};
+
+export default Home;
